332e40bafe8e3718bda4ef915de1ee6b0153d618
Deep Patel <pateldee@SYNOVERGETECH.COM>
2023-06-06 15:33:39 +0530

    Retry fail tests, Screenshot & Video On Test Failure

---
+++ test-runner/playwright.config.ts
@@ -0,0 +1,13 @@
+import { PlaywrightTestConfig } from "@playwright/test";
+
+const config:PlaywrightTestConfig = {
+    use:{
+        headless:false,
+        channel:"chrome",
+        screenshot:"only-on-failure",
+        video:"retry-with-video",
+    },
+    retries:2,
+    reporter:[["dot"],["json",{outputFile:"test-result.json"}]]
+}
+export default config
