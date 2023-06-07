d91fdc6d9d5668835db88577d91cdd5ae1088f9b
Deep Patel <pateldee@SYNOVERGETECH.COM>
2023-06-07 11:52:56 +0530

    HTTP Authentication

---
+++ test-runner/network/auth.ts
@@ -0,0 +1,27 @@
+import {expect,test} from "@playwright/test"
+
+test.describe("Netwrok - Playwright",()=>{
+    test("Http Authentication",async({browser})=>{
+        const context = await  browser.newContext({
+            // proxy:{
+            //     server:"",
+            //     username:"",
+            //     password:""
+            // },
+            httpCredentials:{
+                username:"admin",
+                password:"admin"
+            }
+        })
+        const page = await context.newPage()
+        await page.goto("https://the-internet.herokuapp.com/basic_auth")
+        const header = await page.$("h3")
+        if(header)
+        {
+            console.log(await header.textContent())
+            expect(await header.textContent()).toBe("Basic Auth")
+        }
+        await new Promise(() => {}); // prevents your script from exiting! 
+        await browser.close();
+    })
+})
